4b781ef52231b61a5566f08d387076df63d8efe3
michi260 <michiru260@docomo.ne.jp>
2025-03-02 12:54:50 +0900

    Add files via upload

---
+++ kadai_15/Car_Chapter15.java
@@ -0,0 +1,39 @@
+package kadai_015;
+
+		class Car_Chapter15 {
+		private int gear =1;
+		private int speed =10;
+		
+		
+	
+	public Car_Chapter15 (int gear, int speed) {
+		
+		this.gear = gear=1;
+		this.speed =speed=10;
+		
+		
+	}
+	
+//メソッド
+	public void gearChange(final int afterGear) {
+		this.gear = afterGear;
+		this.speed = switch(this.gear){
+		
+		case 1 -> 10;
+		case 2 -> 20;
+		case 3 -> 30;
+		case 4 -> 40;
+		case 5 -> 50;
+		default ->10;
+		
+		};
+		
+	}
+		
+		
+	public void run() {
+		System.out.println("ギア1から"+this.gear +"に切り替えました");
+		System.out.println("速度は時速"+this.speed+"kmです");
+	}
+		
+	}
